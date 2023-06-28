98ad606809d45a912ec819842eb0836da9d4c235
Theodore Hua <contact@theodorehua.dev>
2023-06-27 20:20:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Reddit Mirror Edit
+This is a tool designed to meet my own needs, which allows for the mass editing of Reddit comments,
+while also creating a copy of your comments locally, and editing the comments with a link to (your own) site where you
+can choose to host the comments yourself. This allows for the removal of your data, without causing a detriment
+to the actual community.
+
+This program isn't extremely polished, as it was originally designed for my own use. I've decided to release it in case
+anyone else finds it useful. If you have any issues, feel free to open an issue, or submit a pull request.
+
+## How does it work?
+The tool attempts to find all of your comments on Reddit (using different sorts, and running multiple times), gets the
+content of the comments, stores a copy of the comments locally, and then edits the comment with your own text.
+
+## How are the comments stored?
+To prevent a bunch of duplicate comments from wasting space, all comments are stored in flat files (MD or HTML files),
+with the name being a SHA-256 hash of the comment's content. If another comment exists with the same content, it will
+not be stored again. This comes with the inherent risk of hash collisions, but the risk is low enough for my purposes.
+
+A main JSON file is also created, which contains all comment's IDs, and the corresponding SHA-256 hash of the comment's
+content. This can be used if you intend of parsing the data in another program or in some other way.
+
+## Frequently Asked Questions
+### Does this work with submissions?
+Not at the moment, I made this for personal use, and I don't have many submissions. You are free to fork and add 
+submission support yourself.
+
+### How can I host my comments?
+The program supports you editing your comments with anything you like, including allowing placeholders for the comment
+ID and/or the comment's hash. This should work for most configurations. You can also choose to create your own dynamic
+site that contains a search function (in which case just link to the main page and don't use any placeholder).
+For my uses, I am hosting all comments on an S3 instance, in their hashed filename, HTML form. All comments are then
+edited to contain a link to the S3 instance, using the hash placeholder to link to the correct comment.
+
+### Will this get every comment?
+Unfortunately, Reddit only allows you to get your past 1000 comments. Although this tool attempts to get more by
+using different sorting patterns (which may show additional comments), it is not guaranteed to get all of them.
+
+You can request a copy of your data from Reddit, which should include all of your comments. This would in theory
+allow you to use this tool to get everything, but I am still waiting on my data request at the time of writing this,
+so the feature is not yet implemented. It will be added in the future.
+
+### I'm running this program again, how do I make sure it doesn't get my already edited comments?
+Pass in as much as your edit text as you can into the string whitelist. The functionality for passing in a JSON
+file of comment IDs to ignore is not yet implemented, but may be in the future.
---
+++ RedditMirrorEdit.py
@@ -0,0 +1,246 @@
+import argparse
+from getpass import getpass
+from hashlib import sha256
+from itertools import zip_longest
+from json import dump
+from os import environ, mkdir
+from os.path import isfile, isdir
+from re import compile
+from uuid import uuid4
+
+import praw
+import praw.models
+from prawcore import OAuthException
+from alive_progress import alive_bar
+from dotenv import load_dotenv
+
+EXPECTED = [
+    "CLIENT_ID",
+    "CLIENT_SECRET",
+]
+
+
+def merge_iterators(*iterators):
+    for i in zip_longest(*iterators, fillvalue=None):
+        for j in i:
+            if j is not None:
+                yield j
+
+
+if __name__ == "__main__":
+    # Configure argparse
+    parser = argparse.ArgumentParser(
+        description="Mass download and edit your Reddit comments"
+    )
+    parser.add_argument(
+        "edit_text",
+        type=str,
+        help="What to replace your comments with. Valid placeholders are: '%{hash}', '%{id}' (without single quotes).",
+    )
+    parser.add_argument(
+        "-w",
+        "--whitelist",
+        action="append",
+        help="Whitelist any comments containing this string (case sensitive)",
+    )
+    parser.add_argument(
+        "-wr",
+        "--whitelist-regex",
+        action="append",
+        help="Whitelist any comments containing a match to this regex",
+    )
+    parser.add_argument(
+        "-ws",
+        "--whitelist-sub",
+        action="append",
+        help="Whitelist any comments in this subreddit (case insensitive)",
+    )
+    parser.add_argument(
+        "-y",
+        "--yes",
+        action="store_true",
+        help="Skip confirmation prompt (this will NOT give you a chance to preview the comments that will be edited)",
+    )
+    parser.add_argument(
+        "--html",
+        action="store_true",
+        help="Store the comments in HTML format instead of Markdown",
+    )
+    parser.add_argument(
+        "--map-save-interval",
+        type=int,
+        help="How often to save the map file (in comments). 0 to save at the end of all parsing. Default: 10",
+        default=10,
+    )
+    parser.add_argument(
+        "--edit-twice",
+        action="store_true",
+        help="Edit the comment twice for the theoretical singular backup Reddit keeps",
+    )
+
+    parse = parser.parse_args()
+
+    # Load environment variables
+    try:
+        load_dotenv()
+    except FileNotFoundError:
+        pass
+
+    if any(i not in environ or len(environ[i].strip()) == 0 for i in EXPECTED):
+        print(
+            "Missing one or more environment variables. Expected: {}".format(
+                ", ".join(EXPECTED)
+            )
+        )
+        exit()
+
+    # Check for prerequisites
+    if not isdir("jobs"):
+        mkdir("jobs")
+
+    JOB_ID = str(uuid4())
+
+    try:
+        username = input("Username (wo/ u/ prefix): ").strip()
+        password = getpass("Password: ")
+        two_factor_code = input("Enter your 2FA Code (if none, leave blank): ").strip()
+        if len(two_factor_code) > 0:
+            password += ":" + two_factor_code
+        reddit = praw.Reddit(
+            client_id=environ["CLIENT_ID"],
+            client_secret=environ["CLIENT_SECRET"],
+            user_agent="RedditMirrorEdit Job {}".format(JOB_ID),
+            username=username,
+            password=password
+        )
+        del password
+        reddit.validate_on_submit = True
+        me: praw.models.Redditor = reddit.user.me()
+    except OAuthException as e:
+        print("Invalid credentials, check your username, password, and/or 2FA code")
+        exit()
+
+    MAP_FILE = "jobs/{}/map.json".format(JOB_ID)
+    FILE_EXTENSION = ".html" if parse.html else ".md"
+    mkdir("jobs/{}".format(JOB_ID))
+    print("Job ID: {}".format(JOB_ID))
+
+    # Initiate runtime variables
+    map_dict = {}
+    to_edit = {}
+    comments_since_last_save = 0
+    whitelist_strings = parse.whitelist if parse.whitelist else []
+    whitelist_regexes = (
+        [compile(i) for i in parse.whitelist_regex] if parse.whitelist_regex else []
+    )
+    whitelist_subs = (
+        [i.lower() for i in parse.whitelist_sub] if parse.whitelist_sub else []
+    )
+
+    # Save all discovered comments
+    with alive_bar(unknown="stars") as bar:
+        for comment in merge_iterators(
+            me.comments.controversial(limit=None),
+            me.comments.hot(limit=None),
+            me.comments.new(limit=None),
+            me.comments.top(limit=None),
+        ):
+            comment: praw.models.Comment
+
+            # Skips
+            if comment.id in map_dict:
+                continue
+            if comment.subreddit.display_name.lower() in whitelist_subs:
+                continue
+            if any(i in comment.body for i in whitelist_strings):
+                continue
+            if any(i.search(comment.body) for i in whitelist_regexes):
+                continue
+
+            # Prerequisite Content
+            body = comment.body_html if parse.html else comment.body
+            comment_hash = sha256(body.encode("utf-8")).hexdigest()
+            filename = "jobs/{}/{}.{}".format(JOB_ID, comment_hash, FILE_EXTENSION)
+
+            # Save
+            map_dict[comment.id] = comment_hash
+            to_edit[comment.id] = comment
+            if not isfile(filename):
+                with open(filename, "w", encoding="utf-8") as f:
+                    f.write(body)
+
+            # Save Map
+            if comments_since_last_save != 0:
+                if comments_since_last_save >= parse.map_save_interval:
+                    with open(MAP_FILE, "w", encoding="utf-8") as f:
+                        dump(map_dict, f)
+                    comments_since_last_save = 1
+                else:
+                    comments_since_last_save += 1
+
+            # Progress Bar
+            bar()
+
+    # Save Map
+    with open(MAP_FILE, "w", encoding="utf-8") as f:
+        dump(map_dict, f)
+
+    # Sanity Check
+    if len(map_dict) != len(to_edit):
+        print(
+            "Sanity check failed, the number of comments in the map file ({}) does not match the number of comments to edit ({})".format(
+                len(map_dict), len(to_edit)
+            )
+        )
+        exit()
+
+    # Confirmation
+    if not parse.yes:
+        num = str(len(to_edit))
+        print(
+            "Found {} comments to edit, to continue, enter the number of comments, 'n'/'N' to cancel.".format(
+                num
+            )
+        )
+        print(
+            "Note: It is recommended to view the comments that will be edited (by looking at the files in the jobs/{}/ directory) before continuing.".format(
+                JOB_ID
+            )
+        )
+        while True:
+            inp = input().strip()
+            if inp == num:
+                break
+            elif inp.lower() == "n":
+                print("Exiting...")
+                exit()
+            else:
+                print("Invalid input, try again.")
+
+    # Edit
+    with alive_bar(len(to_edit)) as bar:
+        for comment_id, comment in to_edit.items():
+            comment: praw.models.Comment
+
+            # Prerequisite Content
+            comment_hash = map_dict[comment_id]
+            replacement_message = parse.edit_text.replace(
+                "%{hash}", comment_hash
+            ).replace("%{id}", comment_id)
+
+            # Edit
+            if parse.edit_twice:
+                comment.edit(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget magna fermentum."
+                )
+            comment.edit(replacement_message)
+
+            # Progress Bar
+            bar()
+
+    # Done
+    print(
+        "Done! You can find the edited comments in the jobs/{}/ directory".format(
+            JOB_ID
+        )
+    )
